9e0f53bd5d2436c61c3a6cb5fbec961a59575e9d
Mahyar Lotfi <mahyarlotfi@gmail.com>
2022-10-24 18:39:16 +0330

    Add login Page to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from unicodedata import name
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template ,url_for
+from flask import render_template ,url_for ,flash ,redirect ,request
 from app import app
+from app.forms import LoginForm
+
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
