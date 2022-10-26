0832936834d479b052d5658e3cfc3099d6e069b0
Mahyar Lotfi <mahyarlotfi@gmail.com>
2022-10-26 17:56:17 +0330

    Add login, register and logout to project

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '63f6fe797026d794e0dc3e2bd279aee19dd2f8db67488172a644bb68792a570c'
-    SQLa
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 # ... add more variables here as needed
